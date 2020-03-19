51ff87c9537609918a3ddfd93211b27b9418e0af
D0753574 <abc895710@gmail.com>
2020-03-19 13:52:36 +0800

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,23 @@
+public class Hello {
+
+	public static void main (String[] args) {
+
+		System.out.println("大家好，我是：");
+
+		System.out.print("姓名：廖振凱，");
+
+		System.out.println("學號：D0753574");
+
+		int a = 100
+		        + 200;
+
+		System.out.printf("%d%n", a);
+
+	}
+
+
+}
+
+
+
+
